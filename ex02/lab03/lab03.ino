22ecff662d4c0606755fa0d1ba52af439b8950e4
shwusnxn <quqichen1@163.com>
2026-05-20 18:43:26 +0800

    lab03:用  函数控制LED以1Hz的频率稳定闪烁

--- ex02/lab03/lab03.ino
+++ ex02/lab03/lab03.ino
@@ -2,29 +2,28 @@
 const int ledPin = 2;  
 
 // 设置PWM属性
-const int freq = 5000;          // 频率 5000Hz
+const unsigned long interval = 500;  // 500ms = 0.5秒，即半个周期
 const int resolution = 8;       // 分辨率 8位 (0-255)
+
+
+unsigned long previousMillis = 0;
+bool ledState = false;
 
 void setup() {
   Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, ledState);
 
-  // 【新版用法】直接将引脚、频率和分辨率绑定
-  // 它会自动返回一个关联的通道（如果需要的话）
-  ledcAttach(ledPin, freq, resolution);
 }
 
 void loop() {
-  // 逐渐变亮
-  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
-    // 【新版用法】直接通过引脚号写入，不再需要指定通道
-    ledcWrite(ledPin, dutyCycle);   
-    delay(1);
-  }
-
-  // 逐渐变暗
-  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
-    ledcWrite(ledPin, dutyCycle);   
-    delay(10);
+  unsigned long currentMillis = millis();
+  
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;  // 保存当前时间
+    
+    ledState = !ledState;            // 翻转LED状态
+    digitalWrite(ledPin, ledState);  // 更新LED
   }
   
   Serial.println("Breathing cycle completed");
